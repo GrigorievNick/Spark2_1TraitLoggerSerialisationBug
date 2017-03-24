142259f9e2a1ed8dfe977997f4db67c71d227e5d
myhr <mykola.hryhoriev@playtech.com>
2017-03-24 11:13:11 +0200

    add println to second case

--- src/main/scala/ReproduceBugMain.scala
+++ src/main/scala/ReproduceBugMain.scala
@@ -5,7 +5,6 @@ import scala.concurrent.duration._
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
-import org.apache.spark.streaming.{Duration, StreamingContext}
 
 /**
   * Created by myhr on 24.03.17.
@@ -15,9 +14,9 @@ object ReproduceBugMain extends TraitWithMethod {
   Logger.getLogger("org").setLevel(Level.OFF)
 
   def main(args: Array[String]): Unit = {
-    val ssc = new StreamingContext(new SparkConf().setMaster("local[4]").setAppName("test"), Duration(1000))
+    val sc = new SparkContext(new SparkConf().setMaster("local[4]").setAppName("test"))
 
-    new ConstantInputDStream(ssc, ssc.sparkContext.parallelize(1 to 100))
+    new ConstantInputDStream(ssc, sparkContext.parallelize(1 to 100))
       .foreachRDD(rdd => {
         implicit val ec = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(1))
         Await.ready(
